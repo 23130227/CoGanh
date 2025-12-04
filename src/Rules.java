9d047d320eaa40a4ea62dbeea4dd806cd7864148
Lưu Quốc Pháp <23130227@st.hcmuaf.edu.vn>
2025-12-05 01:08:22 +0700

    Sua rule

--- src/Rules.java
+++ src/Rules.java
@@ -1,6 +1,7 @@
 // java
 public final class Rules {
-    private Rules() {}
+    private Rules() {
+    }
 
     public static boolean isAdjacentAndConnected(int r1, int c1, int r2, int c2) {
         int dr = Math.abs(r1 - r2);
@@ -9,11 +10,12 @@ public final class Rules {
         if (dr + dc == 1) return true;
 
         if (dr == 1 && dc == 1) {
-            int sum1 = r1 + c1, sum2 = r2 + c2;
-            int diff1 = r1 - c1, diff2 = r2 - c2;
-
-            if (sum1 == sum2 && (sum1 == 2 || sum1 == 4 || sum1 == 6)) return true;
-            if (diff1 == diff2 && (diff1 == -2 || diff1 == 0 || diff1 == 2)) return true;
+//            int sum1 = r1 + c1, sum2 = r2 + c2;
+//            int diff1 = r1 - c1, diff2 = r2 - c2;
+//
+//            if (sum1 == sum2 && (sum1 == 2 || sum1 == 4 || sum1 == 6)) return true;
+//            if (diff1 == diff2 && (diff1 == -2 || diff1 == 0 || diff1 == 2)) return true;
+            if (((r1 + c1) % 2) == 0 && ((r2 + c2) % 2) == 0) return true;
         }
         return false;
     }
